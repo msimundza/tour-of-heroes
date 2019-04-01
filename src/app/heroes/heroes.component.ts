1ecefbc3462918e5bc6a5b815f9bd060fe6d34eb
Marko Simundza <marko.simundza@teckro.com>
2019-04-01 14:58:25 +0100

    Finished Angular Tour of Heroes tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -19,13 +16,22 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-}
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+}
